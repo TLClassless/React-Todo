4d5247dcb946957b56d3ec20304ebf586fb48f22
Ben <classle55sc2@gmail.com>
2020-11-11 12:48:23 +0000

    Adding functionality
    Added checkbox, delete and state functionality, React is kinda wild, eh?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos.js';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Complete Task 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Complete Task 2',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Complete Task 3',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete  
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed // toggle
+      }
+      return todo;
+    }) })
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }) // ... = spread operator, copy all elements already present in [] / filter will filter out todos !== that aren't associated with the selected id
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
